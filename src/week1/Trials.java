3c1718d915e232df91df457032572b4d8ccefa74
kelvinokumu <oumakelvinokumu@gmail.com>
2026-01-23 12:14:06 +0300

    get input

---
+++ src/week1/Trials.java
@@ -0,0 +1,16 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Trials {
+    static void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = br.readLine();
+    }
+}
